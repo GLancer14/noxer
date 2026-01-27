2b86a075564e7cd5ea596efc8af54641506f4eda
unknown <181259602+GLancer14@users.noreply.github.com>
2026-01-27 20:03:32 +0400

    Change BrowserRouter to HashRouter
    CHange name of Marks.ts file

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import { BrowserRouter } from 'react-router'
+import { HashRouter } from 'react-router'
 import { Provider } from 'react-redux'
 import { setupStore } from './store/index.ts'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={setupStore}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </StrictMode>,
 )
